0a948331a309030e172e34a9eebdf5d222717678
OscarSergent <150020702+OscarSergent@users.noreply.github.com>
2023-11-06 12:07:23 +0100

    Update main.js
    cleaning up

--- react/main.js
+++ react/main.js
@@ -1,6 +1,6 @@
 // Faire un tableau de tout les éléments .card dans document
 const Cards = document.querySelectorAll(".card");
-console.log(Cards)
+
 
 // add event(si click donc fonction) à chaque élément du tableau
 Cards.forEach((Card) => {
@@ -10,7 +10,6 @@ Cards.forEach((Card) => {
 // select élément .hidden dans élément card activé        
         const hidden = Card.querySelector(".hidden");
 
-        console.log(hidden)
 
 // overwrite css display de l'élément .hidden
         hidden.style.display = "block";
